28b1dd06ed412f84aaf83137f5be93b568f10f50
jaerenee <jae.renee2707@gmail.com>
2024-02-10 14:31:10 -0600

    Added date and apikey key cycle

--- script.js
+++ script.js
@@ -2,14 +2,30 @@ function on() {
     console.log("L");
     
 }
-var apiKey = "65c7bdbf4bc0e34e403d7525";
-var tripType = "oneway"
 
- function fetchFlightInfo() {
+var tripType = "onewaytrip";
+var apiKey = "";
+var departAirport = "HEL";
+var arrivalAirport = "OUL";
+var desiredDate = "2024-05-20";
+var numAdults = "1";
+var numKids = "0"
+var numBabies = "0";
+var type = "Economy";
+var currency = "USD";
+
+function cycleKeys() {
+    keyBank = ["65c7bdbf4bc0e34e403d752565c7bdbf4bc0e34e403d7525"];
+    ran = Math.floor(Math.random() * 1);
+    apiKey = keyBank[ran];
+}
+
+function fetchFlightInfo() {
     // Define the API URL
-     const apiUrl = "https://api.flightapi.io/" + "/" + apiKey + "/HEL/OUL/2024-05-20/1/0/0/Economy/USD";
+    cycleKeys();
+    const apiUrl = "https://api.flightapi.io/" + tripType + "/" + apiKey + "/" + departAirport + "/" + arrivalAirport + "/" + desiredDate + "/" + numAdults + "/" + numKids + "/" + numBabies + "/" + type + "/" + currency;
 
-        fetch(apiUrl)
+    fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             console.log("Flight Info:", data);
@@ -19,3 +35,5 @@ var tripType = "oneway"
         });
 }
 
+
+// "https://api.flightapi.io/onewaytrip/5f8b1ec2a9d31578961b4109f4dfd8/HEL/OUL/2024-05-20/1/0/0/Economy/USD"
