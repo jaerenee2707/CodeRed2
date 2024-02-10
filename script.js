ebc9ef2d8fe82ae3556632aaed0a81bb7be55c7c
jaerenee <jae.renee2707@gmail.com>
2024-02-10 16:20:11 -0600

    Makes price output

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function on() {
 }
 
 var tripType = "onewaytrip";
-var apiKey = "";
+var apiKey = "65c7bdbf4bc0e34e403d7525";
 //var departAirport = "HEL";
 var arrivalAirport = "OUL";
 var desiredDate = "2024-05-20";
@@ -13,21 +13,21 @@ var numBabies = "0";
 var type = "Economy";
 var currency = "USD";
 var output = "";
-function cycleKeys() {
-    keyBank = ["65c7bdbf4bc0e34e403d752565c7bdbf4bc0e34e403d7525"];
-    ran = Math.floor(Math.random() * 1);
-    apiKey = keyBank[ran];
-}
+//function cycleKeys() {
+  //  keyBank = ["65c7bdbf4bc0e34e403d752565c7bdbf4bc0e34e403d7525"];
+   // ran = Math.floor(Math.random() * 1);
+    //apiKey = keyBank[ran];
+//}
 
 function fetchFlightInfo() {
 
     var inputDepartureAirport = document.getElementById('departureAirport').value;
 
-    document.getElementById('departureAirportDisplay').innerText = "Departure Airport: " + inputDepartureAirport;
+    //document.getElementById('departureAirportDisplay').innerText = "Departure Airport: " + inputDepartureAirport;
 
 
     // Define the API URL
-    cycleKeys();
+    //cycleKeys();
     const apiUrl = "https://api.flightapi.io/" + tripType + "/" + apiKey + "/" + inputDepartureAirport + "/" + arrivalAirport + "/" + desiredDate + "/" + numAdults + "/" + numKids + "/" + numBabies + "/" + type + "/" + currency;
 
     fetch(apiUrl)
@@ -50,5 +50,9 @@ function fetchFlightInfo() {
 // "https://api.flightapi.io/onewaytrip/5f8b1ec2a9d31578961b4109f4dfd8/HEL/OUL/2024-05-20/1/0/0/Economy/USD"
 
     // itineraries.pricing_options.price.amount
-function printAgain() { console.log("this is output", output) }
-    
+function printAgain() {
+    const priceAmount = output.itineraries[0].pricing_options[0].price.amount;
+
+    console.log("Price Amount:", priceAmount);
+}
+
