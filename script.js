3a41cb540ad9658741dc51460bddb8f7e8e49319
jaerenee <jae.renee2707@gmail.com>
2024-02-10 16:04:04 -0600

    commiting

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function on() {
     console.log("L");
-    
 }
 
 var tripType = "onewaytrip";
-var apiKey = "";
+var apiKey = "65c7bdbf4bc0e34e403d752565c7bdbf4bc0e34e403d7525";
 var departAirport = "HEL";
 var arrivalAirport = "OUL";
 var desiredDate = "2024-05-20";
@@ -13,7 +12,7 @@ var numKids = "0"
 var numBabies = "0";
 var type = "Economy";
 var currency = "USD";
-
+var output = "";
 function cycleKeys() {
     keyBank = ["65c7bdbf4bc0e34e403d752565c7bdbf4bc0e34e403d7525"];
     ran = Math.floor(Math.random() * 1);
@@ -29,6 +28,7 @@ function fetchFlightInfo() {
         .then(response => response.json())
         .then(data => {
             console.log("Flight Info:", data);
+            output = data;
         })
         .catch(error => {
             console.error("Error fetching flight info:", error);
@@ -37,3 +37,7 @@ function fetchFlightInfo() {
 
 
 // "https://api.flightapi.io/onewaytrip/5f8b1ec2a9d31578961b4109f4dfd8/HEL/OUL/2024-05-20/1/0/0/Economy/USD"
+
+    // itineraries.pricing_options.price.amount
+function printAgain() { console.log("this is output", output) }
+    
