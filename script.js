7a3fea2561aa7f4396a22a49d338c7f03d0164e3
jaerenee <jae.renee2707@gmail.com>
2024-02-11 00:35:37 -0600

    no button fixes D:

--- script.js
+++ script.js
@@ -21,15 +21,15 @@ var output = "";
 
 function fetchFlightInfo() {
 
+    showLoadingScreen();
     var inputDepartureAirport = document.getElementById('departureAirport').value;
     var inputArrivalAirport = document.getElementById('arrivalAirport').value;
     var inputDate = document.getElementById('Date').value;
     var inputCurrency = document.getElementById('currency').value;
 
-
-
     // Define the API URL
     //cycleKeys();
+
     const apiUrl = "https://api.flightapi.io/" + tripType + "/" + apiKey + "/" + inputDepartureAirport + "/" + inputArrivalAirport + "/" + inputDate + "/" + numAdults + "/" + numKids + "/" + numBabies + "/" + type + "/" + inputCurrency;
 
     fetch(apiUrl)
@@ -65,32 +65,12 @@ function printAgain() {
 
 }
 
-var btnDown = document.getElementById("downButton");
-btnDown.onclick = scrollDown()
+var btnDown = document.getElementById("down");
+btnDown.onclick = scrollDown
 
-var arrDown = document.getElementById("downArrow");
-arrDown.onclick = scrollDown()
 
 function scrollDown() {
     window.scroll(0, window.scrollY + 1000);
     console.log("click")
-}
-
-//Header scroll
-//window.onscroll = function () { myFunction() };
-
-// Get the header
-//var header = document.getElementById("header");
-
-// Get the offset position of the navbar
-//var sticky = header.offsetTop;
-
-//// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-//function myFunction() {
-//    if (window.pageYOffset > sticky) {
-//        header.classList.add("sticky");
-//    } else {
-//        header.classList.remove("sticky");
-//    }
-// }
-
+    window.scrollBy(0, 1000);
+};
